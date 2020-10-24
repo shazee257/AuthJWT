636af74fdd3eee1e3b5cea35d4ef1df9c12078a0
Muhammad Shahzad Bhatti <muhammad.2941906@talmeez.pk>
2020-10-24 21:38:32 +0500

    JWT Completed

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the API" });
+  res.json({ message: "Welcome to the JSON Web Token Tutorial API" });
 });
 
 app.post("/posts", verifyToken, (req, res) => {
-  res.json({ message: "Post created.." });
+  res.json({ message: "Post created!" });
+  console.log(req);
 });
 
 app.post("/login", (req, res) => {
@@ -21,16 +22,37 @@ app.post("/login", (req, res) => {
     username: "shahzad",
     email: "shazee257@gmail.com",
   };
-
+  // Create JWT token
   jwt.sign({ user }, "secretkey", (err, token) => {
-    res.json({ token: token, length: token.length });
+    res.json({ token: token });
   });
 });
 
 // Verify Token
 function verifyToken(req, res, next) {
-  //
-  next();
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    //req.token = bearerToken;
+    // Next middleware
+    jwt.verify(bearerToken, "secretkey", (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        console.log(authData);
+      }
+    });
+    next();
+  } else {
+    // forbidden
+    res.sendStatus(403);
+  }
 }
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
